7eded83bf54deb20bfdbc4b97590376de60cf508
DarleneLamboy <117726080+DarleneLamboy@users.noreply.github.com>
2023-01-27 16:04:34 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -57,7 +57,16 @@ while menu == True:
 
 ###########################          is able to output  if current year is a leapyear
     elif choice == "8":
-        print("")
+        import random
+        jokes = ["What is Forrest Gump´s e-mail password? 1forrest1" , 
+        "Did you hear about the guy who invented the knock knock joke? He won the 'no-bell' price", 
+        "What´s red and bad for your teeth? A brick","What kind of tea is hard to swallow? Reality", "What´s the most terrifying word in nuclear physics? Oooops ",
+        "Ebay is so useless. I tried to look up lighters and all they had were 13 765 matches"]
+        user = input("Press 1 to get a joke")
+
+        if user == "1":
+            print(random.randint(0,(jokes.__len__()-1)))
+            print(jokes[random.randint(0,(jokes.__len__()-1))])
     
 
 
