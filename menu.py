29a74ef70d182a67fb3374bb111a1be9c9ff9c16
Mathias <mathiasgrosse@gmail.com>
2023-01-27 16:46:28 +0100

    Ready, pull and pushh boiiiiiss

--- menu.py
+++ menu.py
@@ -4,16 +4,16 @@ import datetime
 menu = True
 while menu == True:
     print("(1) - #Display Time")
-    print("(2) - #task")
+    print("(2) - #Display Time in Unix")
     print("(3) - #task")
     print("(4) - #task")
     print("(5) - #task")
     print("(6) - #task")
     print("(7) - #task")
     print("(8) - #task")
-    choice = input("ENTER CHOICE: (1-8)")
+    choice = input("ENTER CHOICE(1-8):")
 
-#############################   
+    # Display time (Mathias)   
     if choice == "1":
         current_time = datetime.datetime.now().time()
         Display_time = current_time.strftime("%H:%M:%S")
